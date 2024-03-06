f15c04734d827970b8d01b5b13f46eed32c09a8e
Andrey Skazov <skazov.andrey.by@gmail.com>
2024-03-06 19:29:54 +0300

    FenDL structure

--- include/Layer.hxx
+++ include/Layer.hxx
@@ -5,9 +5,14 @@
 #ifndef FENDL_LAYER_HXX
 #define FENDL_LAYER_HXX
 
+#include <Eigen>
+
+using namespace Eigen;
 
 class Layer {
-
+public:
+    size_t _size;
+    MatrixXd active_values,values,gradient,weights;
 };
 
 
